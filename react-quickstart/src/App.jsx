b08510530aa9baf182486ca3637cf3ff7a0eefa8
trishaolorvida <trisha.olorvida0207@gmail.com>
2024-10-24 00:49:42 +0800

    I’m done completing the section on how to share data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,16 +1,16 @@
-function MyButton() {
+export default function MyApp() {
   const [count, setCount] = useState(0);
-
+ 
   function handleClick() {
     setCount(count + 1);
   }
-
+ 
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
   );
-}
-
-
-
+ }
+ 
